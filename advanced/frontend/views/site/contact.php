428457a377355e2b12bb8d003655a2d4b964eba9
lucaslomba <lucasazevedolomba@gmail.com>
2022-07-08 15:38:17 -0300

    Adicionando Pretty URL

--- advanced/frontend/views/site/contact.php
+++ advanced/frontend/views/site/contact.php
@@ -13,7 +13,7 @@ $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-contact">
     <!-- Page Header -->
-    <header class="masthead" style="background-image: url('img/home-bg.jpg')">
+    <header class="masthead" style="background-image: url('../img/home-bg.jpg')">
       <div class="container">
         <div class="row">
           <div class="col-lg-8 col-md-10 mx-auto">
